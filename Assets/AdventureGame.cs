a529f1ffd7ea1017f3787350beaf5787e800bf2b
Hannah <hannah.asis@obf.ateneo.edu>
2020-07-03 05:18:31 +0800

    first commit from Lesson 28

---
+++ Assets/AdventureGame.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour {
+
+    [SerializeField] Text textComponent;
+    [SerializeField] State startingState;
+
+    State state;
+
+	// Use this for initialization
+	void Start () {
+        state = startingState;
+        textComponent.text = state.GetStateStory();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ManageState();
+	}
+
+    private void ManageState()
+    {
+        var nextStates = state.GetNextStates();
+        if (Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            state = nextStates[0];
+            
+        }
+        else if (Input.GetKeyUp(KeyCode.LeftArrow))
+        {
+            state = nextStates[1];
+        }
+        else if (Input.GetKeyUp(KeyCode.UpArrow))
+        {
+            state = nextStates[2];
+        }
+
+        textComponent.text = state.GetStateStory();
+    }
+}
